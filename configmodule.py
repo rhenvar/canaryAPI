0373c94e0891411812dc78969a050aeb9d4fc2f0
rhenvar <garrickkrol@hotmail.com>
2019-12-28 11:42:41 -0800

    Config - suppressing SqlAlchemyModifications warning

--- configmodule.py
+++ configmodule.py
@@ -2,6 +2,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
